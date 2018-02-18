7b330d48abec7ee0607886582cc06a1d7379afa7
hanghang177 <weihang@illinois.edu>
2018-02-18 13:56:22 -0600

    main function logistics

---
+++ robo-i-star/main.py
@@ -0,0 +1,30 @@
+import dronekit
+import pygame
+import LocationAudio
+
+
+
+def init():
+    # initialize the vehicle and other modules
+    robot = Navigator()
+
+if __name__ == "__main__":
+    # Main function
+
+    # User Interface
+        # Get User click on map to generate destination
+        # Waypoint generate & upload to Ardurover
+
+    # ROS
+        # Get obstacles
+        # If too close --> Manual mode w/ pwm instructions
+        # If far enough --> Auto
+
+    # Ardurover GPS
+        #Ardurover Get Location(Waypoint)
+        #Ardurover Play Audio
+
+    # GPS Navigation
+        # Done in Ardurover
+
+    pass
