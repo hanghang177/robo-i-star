12dbfbd227c780b742deb1264ecea0e6928186eb
Ryan Plyman <rplyman2@illinois.edu>
2018-02-28 20:53:43 -0600

    Latest updates to main.py and LocationAudio.py

--- robo-i-star/main.py
+++ robo-i-star/main.py
@@ -28,39 +28,39 @@ if __name__ == "__main__":
         navigator.setLocationindex(targetbuildingindex)
         print ("The target location is: " + navigator.gettargetlocation())
 
-        currentmision = navigator.getcurrentmission()
-    navigator.upload_mission(currentmission)
+        currentmission = navigator.getcurrentmission()
+        navigator.upload_mission(currentmission)
 
-    # Need to integrate mission control with obstacle avoidance
+        # Need to integrate mission control with obstacle avoidance
 
-    # while no obstacle detected --> run automated mission control
-    navigator.run_mission()
+        # while no obstacle detected --> run automated mission control
+        navigator.run_mission()
 
-    # if obstacle detected,
-    # navigator.pause_mission()
-    # recheck for obstacle and when its clear,
-    # navigator.continue misssion()
+        # if obstacle detected,
+        # navigator.pause_mission()
+        # recheck for obstacle and when its clear,
+        # navigator.continue_mission()
 
-    # implementing a timer in order to add a period to gather around robot when mission is complete
-    time.sleep(10)  # 10 second delay
+        # implementing a timer in order to add a period to gather around robot when mission is complete
+        time.sleep(10)  # 10 second delay
 
-    currentaudio = navigator.getaudio()
-    navigator.PlayAudio(currentaudio)  # Plays audio file at current location
-    time.sleep(5)  # 5 second delay after audio plays
+        currentaudio = navigator.getaudio()
+        navigator.PlayAudio(currentaudio)  # Plays audio file at current location
+        time.sleep(5)  # 5 second delay after audio plays
 
-    # Need to start return mission whose file is located at current mission + 6
-    navigator.setLocationindex(targetbuildingindex + 6)
-    currentmision = navigator.getcurrentmission()
+        # Need to start return mission whose file is located at current mission + 6
+        navigator.setLocationindex(targetbuildingindex + 6)
+        currentmission = navigator.getcurrentmission()
 
-    # while no obstacle detected --> run automated mission control
-    navigator.upload_mission(currentmission)
+        # while no obstacle detected --> run automated mission control
+        navigator.upload_mission(currentmission)
 
-# if obstacle detected,
-# navigator.pause_mission()
-# recheck for obstacle and when its clear,
-# navigator.continue misssion()
+        # if obstacle detected,
+        # navigator.pause_mission()
+        # recheck for obstacle and when its clear,
+        # navigator.continue_mission()
 
-# implementing a timer in order to add a period to gather around robot when mission is complete
-time.sleep(5)  # 5 second delay
+        # implementing a timer in order to add a period to gather around robot when mission is complete
+        time.sleep(5)  # 5 second delay
 
-# Restart a new mission by looping back to top, while loop
+        # Restart a new mission by looping back to top, while loop
